be6a59309b5e0ef64bb77051bdcd12fd3dbbcf35
AntonKantardzhiev <kantarjiev88@abv.bg>
2021-03-16 11:19:44 +0100

    Adet some blank page

---
+++ Templates/header.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<header>
+    <h1>School of many</h1>
+    <h2>Welcome to aware school</h2>
+    <h3>We are proud to present aware classes/h3>
+</header>
+<body>
