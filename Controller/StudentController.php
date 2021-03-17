503caec9b4420e4c496874eb77c65cfd8c7e6daf
Jens <jns.bys@gmail.com>
2021-03-17 10:42:33 +0100

    update controller, not finished

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -5,13 +5,39 @@ class StudentController
 
     protected function getStudentInfo()
     {
-$connection = new DbConnect();
-$pdo = $connection->connect();
+        $connection = new DbConnect();
+        $pdo = $connection->connect();
 
-$student = studentLoader::getStudent($_GET['id'], $pdo);
-require '?';
+        $student = studentLoader::getStudent($_GET['id'], $pdo);
+        require '?';
     }
 
+    protected function getAllStudentInfo()
+    {
+        $connection = new DbConnect();
+        $pdo = $connection->connect();
+
+        $allStudents = studentLoader::getAllStudents($pdo);
+        require '?';
+    }
+
+    protected function createNewStudent()
+    {
+        $connection = new DbConnect();
+        $pdo = $connection->connect();
+
+        $edit = studentLoader::createStudent($pdo);
+        require '?';
+    }
+
+    protected function editStudent()
+    {
+        $connection = new DbConnect();
+        $pdo = $connection->connect();
+
+        $delete = studentLoader::saveStudent($_GET['id'], $pdo);
+        require '?';
+    }
 
 }
 
