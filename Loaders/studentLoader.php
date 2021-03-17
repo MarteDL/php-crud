2b8189af08eb1fe30ed0e78484d217f6000d765c
Micha <michaelaharin@gmail.com>
2021-03-17 10:12:13 +0100

    Martes changes

--- Loaders/studentLoader.php
+++ Loaders/studentLoader.php
@@ -3,29 +3,27 @@
 
 class studentLoader
 {
-        public static function getStudent(int $id, PDO $pdo) : student
-        {
-            $handle = $pdo->prepare('SELECT firstname, lastname, email, classname FROM student WHERE studentId = :id');
-            $handle->bindValue(':id', $id);
-            $handle->execute();
+    public static function getStudent(int $id, PDO $pdo): student
+    {
+        $handle = $pdo->prepare('SELECT firstname, lastname, email, classname FROM student WHERE studentId = :id');
+        $handle->bindValue(':id', $id);
+        $handle->execute();
 
-            $studentArray = $handle->fetch(PDO::FETCH_ASSOC);
+        $studentArray = $handle->fetch(PDO::FETCH_ASSOC);
 
-            return new student($studentArray['lastname'], $studentArray['firstname'], $studentArray['email'], $studentArray['classname']);
+        return new student($studentArray['lastname'], $studentArray['firstname'], $studentArray['email'], $studentArray['classname']);
+    }
+
+    public static function getAllStudents(PDO $pdo): array
+    {
+        $handle = $pdo->query('SELECT firstname, lastname, email, classname FROM student');
+        $studentsArray = $handle->fetchAll();
+
+        $students = [];
+
+        foreach ($studentsArray as $student) {
+            $students[] = new student($student['lastname'], $student['firstname'], $student['email'], $student['classname']);
         }
-
-        public static function getAllStudents(PDO $pdo): array
-        {
-            $handle = $pdo->query('SELECT firstname, lastname, email, classname FROM student');
-            $studentsArray = $handle->fetchAll();
-
-            $students = [];
-
-            foreach ($studentsArray AS $student) {
-                $students[] = new student($student['lastname'], $student['firstname'], $student['email'], $student['classname']);
-            }
-
-            return $students;
-        }
-
+        return $students;
+    }
 }
